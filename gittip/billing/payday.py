4deb542e4628ade972801226d5761ca77108f8e2
Changaco <changaco@changaco.oy.lc>
2014-06-21 22:57:11 +0200

    rename argument

--- gittip/billing/payday.py
+++ gittip/billing/payday.py
@@ -663,7 +663,7 @@ class Payday(object):
         self.record_exchange('bill', amount, fee, error, participant)
 
 
-    def ach_credit(self, participant, total, minimum_credit=MINIMUM_CREDIT):
+    def ach_credit(self, participant, withhold, minimum_credit=MINIMUM_CREDIT):
 
         # Compute the amount to credit them.
         # ==================================
@@ -672,7 +672,7 @@ class Payday(object):
 
         balance = participant.balance
         assert balance is not None, balance # sanity check
-        amount = balance - total
+        amount = balance - withhold
 
         # Do some last-minute checks.
         # ===========================
@@ -682,9 +682,9 @@ class Payday(object):
 
         if amount < minimum_credit:
             also_log = ""
-            if total > 0:
+            if withhold > 0:
                 also_log = " ($%s balance - $%s in obligations)"
-                also_log %= (balance, total)
+                also_log %= (balance, withhold)
             log("Minimum payout is $%s. %s is only due $%s%s."
                % (minimum_credit, participant.username, amount, also_log))
             return      # Participant owed too little.
@@ -699,9 +699,9 @@ class Payday(object):
         credit_amount, fee = skim_credit(amount)
         cents = credit_amount * 100
 
-        if total > 0:
+        if withhold > 0:
             also_log = "$%s balance - $%s in obligations"
-            also_log %= (balance, total)
+            also_log %= (balance, withhold)
         else:
             also_log = "$%s" % amount
         msg = "Crediting %s %d cents (%s - $%s fee = $%s) on Balanced ... "
